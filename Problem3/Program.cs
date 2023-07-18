9ad6bec70a22bf0d21870541737aedbb804e96e3
Tom Serrante <tserrante@gmail.com>
2023-07-18 18:40:42 -0400

    started problem 3

---
+++ Problem3/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjectEuler.Problem3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            const long number = 600851475143L;
+            
+        }
+    }
+}
