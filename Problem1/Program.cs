cb266281a7f00687ad6ab6581d84a2bff5e3a431
Tom Serrante <tserrante@gmail.com>
2023-07-17 20:04:10 -0400

    initial commit - first problem solved

---
+++ Problem1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace ProjectEuler.Problem1
+{
+    public class Program
+    {
+        // the ranges are non-inclusive in the resulting sum i.e. 1-999 is used
+        const int RANGE_START = 0;
+        const int RANGE_END = 1000;
+        public static void Main(string[] args)
+        {
+            long sum = 0;
+            for(int i = RANGE_START; i < RANGE_END; i++)
+            {
+                if(i % 3 == 0 || i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            System.Console.WriteLine($"Sum:\t{sum}");
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,2 @@
+My solutions to the first 100 problems from Project Euler written in C#
+https://projecteuler.net/
