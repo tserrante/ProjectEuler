2a2f59f8c0a0de432cb1e1f3e54dc5bdc638881a
Tom Serrante <tserrante@gmail.com>
2023-08-05 14:47:28 -0400

    solved problem 5

--- Problem5/Program.cs
+++ Problem5/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace ProjectEuler.Problem5;
+
+public class Program
+{
+    static int startNumber = 1;
+    static int endNumber = 20;
+    public static void Main(string[] args)
+    {
+        int smallestDivisibleNumber = GetSmallestDivisibleNumber_BruteForce(startNumber,endNumber);
+        System.Console.WriteLine($"Smallest Divisible Number by 1-20: {smallestDivisibleNumber}");
+    }
+
+    public static int GetSmallestDivisibleNumber_BruteForce(int start, int end)
+    {
+        int smallestDivisibleNumber = end;
+
+        while(!isDivisibleByRange(start, end, smallestDivisibleNumber))
+        {
+            smallestDivisibleNumber++;
+        }
+        return smallestDivisibleNumber;
+    }
+
+    public static bool isDivisibleByRange(int start, int end, int dividend)
+    {
+        for(int i = start; i <= end; i++)
+        {
+            if(dividend % i != 0)
+                return false;
+        }
+        return true;
+    }
+}
