bea4d6aac491e82f512577a3b6dcc0c0b3c3c854
Tom Serrante <tserrante@gmail.com>
2023-08-06 12:39:43 -0400

    solved problem 6

--- Problem6/Program.cs
+++ Problem6/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace ProjectEuler.Problem6;
+
+public class Program
+{
+    public static int rangeEnd = 100;
+    public static void Main(string[] args)
+    {
+        int sumOfSquares = GetSumOfSquares(rangeEnd);
+        int squareOfSum = GetSquareOfSum(rangeEnd);
+
+        System.Console.WriteLine($"{squareOfSum} - {sumOfSquares} = {squareOfSum - sumOfSquares}");
+
+    }
+    public static int GetSumOfSquares(int rangeEnd)
+    {
+        int sumOfSquares = 0;
+
+        for(int i = 1; (i < rangeEnd+1); i++)
+        {
+            sumOfSquares += (i * i);
+        }
+        return sumOfSquares;
+    }
+    public static int GetSquareOfSum(int rangeEnd)
+    {
+        int squareOfSums = 0;
+
+        for(int i = 1; (i < rangeEnd + 1); i++)
+        {
+            squareOfSums += i;
+        }
+        return squareOfSums * squareOfSums;
+    }
+}
