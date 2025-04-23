a79691308e96cb570643220fb20c0316dca5eadc
Tom S <tserrante@gmail.com>
2025-04-23 12:28:11 -0400

    experimenting

--- Problem12/Program.cs
+++ Problem12/Program.cs
@@ -15,31 +15,35 @@ public class Program
         while(GetNumberOfDivisors(sum) < 500)
         //while(true)
         {
-            Turn();
+            //Turn();
             sum += count;
             count++;
         }
+        //ProgressBar();
         Console.ReadLine();
     }
     public static int GetNumberOfDivisors(int number)
     {
         int numDivisor = 0;
-        Console.Write($"Current Number {number}");
-        Console.WriteLine();
+        Console.Write("Current Number: \r{0}          ", number);
+        //Console.Write("\r{0}          ", number);
+        //Console.WriteLine();
 
         for(int i = 1; i < number / 2; i++)
         {
             if(number % i == 0)
                 numDivisor++;
         }
-        Console.Write($"# of Divisors {numDivisor}");
-        Console.WriteLine();
+        // Console.Write("# of Divisors: ");
+        // Console.Write("\r{0}          ", numDivisor);
+        // Console.WriteLine();
 
         if(largestNumDivisor < numDivisor)
             largestNumDivisor = numDivisor;
         
-        Console.Write($"Current Largest # of Divisors: {largestNumDivisor}");
-        Console.WriteLine();
+        // Console.Write("Current Largest # of Divisors: ");
+        // Console.Write("\r{0}          ", largestNumDivisor);
+        // Console.WriteLine();
 
         //Console.Clear();
         return numDivisor;
@@ -56,5 +60,13 @@ public class Program
         }
         Thread.Sleep(10);
         Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+    }
+    public static void ProgressBar()
+    {
+        for(int i = 0; i < 100; ++i)
+        {
+            Console.Write("\r{0}%   ", i);
+            Thread.Sleep(250);
+        }
     }
 }
